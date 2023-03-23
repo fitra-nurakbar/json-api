4070239b578ea0778f1264699d7553c37ff8641e
fitra-nurakbar <fitranurakbar378@gmail.com>
2023-03-23 15:06:05 +0700

    add access CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const data = require("./data.json");
+const cors = require("cors");
 
 const port = 5000
+
+app.use(cors())
 
 app.get("/", function (req, res) {
   res.json(data);
